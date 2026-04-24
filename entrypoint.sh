69ade9e8eeb2540c7a61d9c9c02f69d9dcea65d3
nikitaWebApps <nikitagolovlev.design@gmail.com>
2026-04-24 10:40:52 +0200

    Add parameter to ignore warnings on exit

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,20 @@ if [ -n "${INPUT_PHPCS_EXTENSIONS}" ]; then
     EXTENSIONS_FLAG="--extensions=${INPUT_PHPCS_EXTENSIONS}"
 fi
 
+IGNORE_WARNINGS_ON_EXIT_FLAG=""
+if [ "${INPUT_IGNORE_WARNINGS_ON_EXIT}" = "true" ]; then
+    echo "Warnings will not cause a non-zero exit code"
+    IGNORE_WARNINGS_ON_EXIT_FLAG="--runtime-set ignore_warnings_on_exit 1"
+fi
+
 if [ -z "${INPUT_ENABLE_WARNINGS}" ] || [ "${INPUT_ENABLE_WARNINGS}" = "false" ]; then
     echo "Check for warnings disabled"
 
-    ${INPUT_PHPCS_BIN_PATH} -n --standard=YNA --report=checkstyle --ignore=${INPUT_PHPCS_IGNORE_PATHS} ${EXTENSIONS_FLAG} ${INPUT_PHPCS_PATHS}
+    ${INPUT_PHPCS_BIN_PATH} -n --standard=YNA --report=checkstyle --ignore=${INPUT_PHPCS_IGNORE_PATHS} ${EXTENSIONS_FLAG} ${IGNORE_WARNINGS_ON_EXIT_FLAG} ${INPUT_PHPCS_PATHS}
 else
     echo "Check for warnings enabled"
 
-    ${INPUT_PHPCS_BIN_PATH} --standard=YNA --report=checkstyle --ignore=${INPUT_PHPCS_IGNORE_PATHS} ${EXTENSIONS_FLAG} ${INPUT_PHPCS_PATHS}
+    ${INPUT_PHPCS_BIN_PATH} --standard=YNA --report=checkstyle --ignore=${INPUT_PHPCS_IGNORE_PATHS} ${EXTENSIONS_FLAG} ${IGNORE_WARNINGS_ON_EXIT_FLAG} ${INPUT_PHPCS_PATHS}
 fi
 
 status=$?
